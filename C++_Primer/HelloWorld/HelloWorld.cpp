85f0705ba4f06faa28ecbfc0e6b8c3fd10ded387
ordinarylinuxuser <kentparker666@gmail.com>
2024-02-27 14:55:24 +0800

    added comments

--- C++_Primer/HelloWorld/HelloWorld.cpp
+++ C++_Primer/HelloWorld/HelloWorld.cpp
@@ -1,7 +1,18 @@
+/**
+ * @file HelloWorld.cpp
+ * @author Ordinary Linux User (kentparker666@gmail.com)
+ * @brief It's a Hello World
+ * @version 0.1
+ * @date 2024-02-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include <iostream>
 
 int main()
 {
+    // Well it's a Hello World what did you expect?
     std::cout << "Hello World" << std::endl;
     return 0;
 }
--- C++_Primer/HelloWorld/IO.cpp
+++ C++_Primer/HelloWorld/IO.cpp
@@ -1,9 +1,32 @@
+/**
+ * @file IO.cpp
+ * @author Ordinary Linux User
+ * @brief Shows how to read and write to input and output buffer.
+ * @version 0.1
+ * @date 2024-02-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include <iostream>
+
 int main()
 {
-    std::cout << "Enter two numbers:" << std::endl;
+    // Print to output Stream
+    std::cout << "Enter two numbers:"
+              << std::endl;
+    // initialize variables
     int v1, v2 = 0;
+
+    // read from input stream
     std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+
+    // print the sum to output stream
+    std::cout << "The sum of "
+              << v1 << " and "
+              << v2 << " is "
+              << v1 + v2
+              << std::endl;
+
     return 0;
 }
